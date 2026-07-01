a287a823121fd941ab75e54e45628744c6533d2d
iki1uc <entertrainer@web.de>
2026-07-01 03:48:08 +0200

    Update primus-core.js

--- primus-core.js
+++ primus-core.js
@@ -1,1 +1,9 @@
+// primus-core.js
+export function initPrimus(root) {
+  root.innerHTML = `
+    <div style="padding:10px;color:#0f0;font-family:monospace;">
+      PRIMUS-Modul aktiv
+    </div>
+  `;
+}
 
