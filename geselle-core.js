125dbcc1bd1fb059cf7fb5bcac781ee3e7c66d76
iki1uc <entertrainer@web.de>
2026-07-01 03:48:23 +0200

    Add initGeselle function to initialize module

--- geselle-core.js
+++ geselle-core.js
@@ -1,1 +1,9 @@
+// geselle-core.js
+export function initGeselle(root) {
+  root.innerHTML = `
+    <div style="padding:10px;color:#0f0;font-family:monospace;">
+      GESELLE-Modul aktiv
+    </div>
+  `;
+}
 
